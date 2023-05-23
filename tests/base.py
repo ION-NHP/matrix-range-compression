910389a90b03bdd7b26679a499a17f65bc288c0b
myuan <zhengmy@ion.ac.cn>
2023-05-23 16:44:18 +0800

    add base test

--- tests/base.py
+++
@@ -1,4 +0,0 @@
-from range_compression import RangeCompressedMask, mask_encode
-
-def test_base():
-    assert 1 == 1
---
+++ tests/test_mask.py
@@ -0,0 +1,55 @@
+import random
+import os
+
+import cv2
+import numpy as np
+
+from range_compression import RangeCompressedMask, mask_encode
+
+
+def test_mask():
+    width, height = 800, 600
+    image = np.zeros((height, width), dtype=np.uint8)
+
+    num_polygons = 5 
+
+    for _ in range(num_polygons):
+        num_sides = random.randint(3, 8)
+        color = random.randint(0, 255)
+        vertices = []
+
+        for _ in range(num_sides):
+            x = random.randint(0, width)
+            y = random.randint(0, height)
+            vertices.append((x, y))
+
+        pts = np.array(vertices, np.int32)
+
+        cv2.fillPoly(image, [pts], color)
+
+    rcm = mask_encode(image)
+    assert isinstance(rcm, RangeCompressedMask)
+    assert rcm.w == width and rcm.h == height
+    assert len(rcm.row_indexes) == height
+    
+    random_n = 10000
+    randx, randy = np.random.randint(0, width, random_n), np.random.randint(0, height, random_n)
+    
+    res = rcm.find_index(randx, randy)
+    actual_res = image[randy, randx]
+    
+    assert np.array_equal(res, actual_res)
+
+    rcm.save('tests/output')
+    rcm2 = RangeCompressedMask.load('tests/output')
+    assert np.array_equal(rcm.encodings[:, :3], rcm2.encodings)
+    
+    actual_res2 = rcm2.find_index(randx, randy)
+    assert np.array_equal(actual_res, actual_res2)
+
+    os.remove('tests/output/encodings.parquet')
+    os.remove('tests/output/row_indexes.parquet')
+    os.remove('tests/output/meta.json')
+    os.rmdir('tests/output')
+
+    
