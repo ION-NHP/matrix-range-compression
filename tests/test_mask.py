a4e00f3eec8fe22501fccd7f363783b7eb7604db
myuan <zhengmy@ion.ac.cn>
2023-05-26 15:27:33 +0800

    add new test

--- tests/test_mask.py
+++ tests/test_mask.py
@@ -6,9 +6,8 @@ import numpy as np
 
 from range_compression import RangeCompressedMask, mask_encode
 
-
-def test_mask():
-    width, height = 800, 600
+def get_rand_image():
+    width, height = 1024, 2048
     image = np.zeros((height, width), dtype=np.uint8)
 
     num_polygons = 5 
@@ -26,28 +25,39 @@ def test_mask():
         pts = np.array(vertices, np.int32)
 
         cv2.fillPoly(image, [pts], color)
+    return image
+
+def _test_encode(image):
+    height, width = image.shape
 
     rcm = mask_encode(image)
     assert isinstance(rcm, RangeCompressedMask)
     assert rcm.w == width and rcm.h == height
     assert len(rcm.row_indexes) == height
-    
+    return rcm
+
+def get_randxy(width, height):
     random_n = 10000
     randx, randy = np.random.randint(0, width, random_n), np.random.randint(0, height, random_n)
-    
+    return randx, randy
+
+def _test_find_mask(rcm, image, randx, randy):
     res = rcm.find_index(randx, randy)
     actual_res = image[randy, randx]
-    
-    try:
-        assert np.array_equal(res, actual_res)
-    except:
-        np.save('tests/image.npy', image)
-        np.save('tests/randx.npy', randx)
-        np.save('tests/randy.npy', randy)
-        np.save('tests/res.npy', res)
-        np.save('tests/actual_res.npy', actual_res)
-        raise
+    assert np.array_equal(res, actual_res)
+    return actual_res
 
+
+def test_mask(benchmark):
+    image = get_rand_image()
+    height, width = image.shape
+
+    rcm = _test_encode(image)
+    randx, randy = get_randxy(width, height)
+
+    benchmark.pedantic(_test_find_mask, args=(rcm, image, randx, randy), iterations=10, rounds=50)
+    
+    actual_res = _test_find_mask(rcm, image, randx, randy)
     rcm.save('tests/output')
     rcm2 = RangeCompressedMask.load('tests/output')
     assert np.array_equal(rcm.encodings[:, :3], rcm2.encodings)
@@ -60,5 +70,3 @@ def test_mask():
     os.remove('tests/output/meta.json')
     os.rmdir('tests/output')
 
-def test_benchmark(benchmark):
-    benchmark(test_mask)
